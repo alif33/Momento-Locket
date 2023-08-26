18387beeeb29462388fafba728433939acaab1d7
alif33 <alifhasan332@gmail.com>
2023-08-26 22:05:47 +0600

    product details

--- src/components/cards/Product.jsx
+++ src/components/cards/Product.jsx
@@ -11,16 +11,21 @@ const Product = () =>{
                 src="/img/product.png"
                 alt="product image"
             />
-            <div className=" flex flex-col justify-center">
-                <h3 className="text-center">Momento® Urn</h3>
-                <h4 className="text-center">$ 300.00</h4>
-                <div className="flex flex-row justify-between">
-                    <button>Silver</button>
-                    <button>Yellow gGold</button>
-                    <button>White Gold</button>
+            <div className=" flex flex-col justify-center pt-5">
+                <h3 className="text-[#747067] text-center text-base font-medium">Momento® Urn</h3>
+                <h4 className="text-[#747067] text-lg font-light text-center py-3">$ 300.00</h4>
+                <div className="flex justify-center gap-3">
+                    <button className="bg-[#74706714] px-3 py-2 rounded-full">Silver</button>
+                    <button className="bg-[#74706714] px-3 py-2 rounded-full">Yellow gGold</button>
+                    <button className="bg-[#74706714] px-3 py-2 rounded-full">White Gold</button>
                 </div>
-                <div>rou</div>
-                <button>Add to Bag</button>
+                <span className="mx-auto my-5">
+                    <svg width="50" height="50" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="25" cy="25" r="21" fill="none" stroke="#747067" stroke-width="1" />
+                        <circle cx="25" cy="25" r="20" fill="#74706726" />
+                    </svg>
+                </span>
+                <button className="btn-submit">Add to Bag</button>
             </div>
         </Link>
     )
--- src/components/product/Details.jsx
+++ src/components/product/Details.jsx
@@ -41,34 +41,49 @@ const Details = () =>{
                         alt="detail product"
                     />
                 <div>
-                    <div>
-                        <span>Metal</span> 
-                        <button>Sliver</button>
-                        <button>Yellow Gold</button>
-                        <button>White Gold</button>
+                    <div className="flex gap-3">
+                        <span className="text-xs font-light my-auto">Metal</span> 
+                        <div className="flex gap-1 text-sm font-light">
+                            <button className="bg-[#74706714] px-5 py-3 rounded-full">Sliver</button>
+                            <button className="bg-[#74706714] px-5 py-3 rounded-full">Yellow Gold</button>
+                            <button className="bg-[#74706714] px-5 py-3 rounded-full">White Gold</button>
+                        </div>
                     </div>
-                    <div>
-                        <span>Colors</span> 
-                        <button>Svg</button>
+                    <div className="flex gap-3">
+                        <span className="text-xs font-light my-auto">Colors</span> 
+                        <span className="min-height-[32px] min-width-[32px] rounded-full"></span>
                     </div>
-                    <div>
-                        <span>Metal</span> 
-                        <span>$300</span>
-                    </div>
-                    <hr/>
-                    <div>
-                        <span>Metal</span> 
-                        <span>$300</span>
+                    <div className="flex gap-3">
+                        <span className="text-xs font-light my-auto">Metal</span> 
+                        <span className="text-[#747067] text-xl font-medium">$ 300</span>
                     </div>
                     <hr/>
-                    <button>UPLOAD YOUR FILE & ORDER NOW</button>
-                    <div>
-                        <span>Not now.</span>
-                        <span>I you can upload photos when reciving the urn</span>
+                    <div className="flex justify-between py-3">
+                        <div className="my-auto">
+                            <span className="text-xs font-light">Quantity:</span> 
+                            <span>
+
+                            </span>
+                        </div>
+                        <button className="btn-submit">
+                            Add to Bag
+                        </button>
+                    </div>
+                    <hr/>
+                    <button className="btn-submit my-5">UPLOAD YOUR FILE & ORDER NOW</button>
+                    <div className="flex gap-5 py-5">
+                        <span className="text-[#996D01] text-2xl font-bold">Not now.</span>
+                        <span className="text-[#996D01] text-base font-medium my-auto">I you can upload photos when reciving the urn</span>
                     </div>
                     <hr/>
                     <ul>
                         <li>
+                            <Image
+                                height={24}
+                                width={24}
+                                src="/icons/"
+                                alt="icon"
+                            />
                             <span>
                                 <h1>Unlimited Sharing</h1>
                                 <h5>Access your memories any time as much as you want. There are no additional fees, access limitations or hidden costs.</h5>
