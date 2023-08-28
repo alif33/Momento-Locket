d81fc0ca8e4c2822a8936b966b4c031ad880eabf
alif33 <alifhasan332@gmail.com>
2023-08-28 21:34:58 +0600

    animation move completed

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Hero = () =>{
     return(
         <div className="bg-cover h-fit bg-hero flex flex-col justify-between">
-            <div className="pulse relative">
+            <div className="pulse">
                 <div className="absolute">
                     <Image
                         width={556}
@@ -14,13 +14,14 @@ const Hero = () =>{
                     />
                 </div>
             </div>
-            <div className="h-[800px] mx-auto">
-                <Image
-                    height={480}
-                    width={240}
-                    src="/img/phone.png"
-                    alt="Mobile Phone"
-                />
+            <div className="h-[800px] w-[240px] mx-auto mt-10">
+                    <Image
+                        height={480}
+                        width={240}
+                        className="move-image"
+                        src="/img/phone.png"
+                        alt="Mobile Phone"
+                    />
             </div>
         </div>
     )
