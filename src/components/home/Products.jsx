651e17d36db7b05164c07c81b78f26a60c10d40b
alif33 <alifhasan332@gmail.com>
2023-09-01 02:11:22 +0600

    product card is finished

--- src/components/home/Products.jsx
+++ src/components/home/Products.jsx
@@ -8,7 +8,7 @@ const Products = () =>{
             <div className="w-10/12 mx-auto py-3">
                 <h1 className="text-gradient text-center trajan-font uppercase text-4xl my-3 py-5">Choose your Heavenly Pearl Gates below.</h1>
                 <h5 className="text-[#747067] text-lg font-light text-center pb-3">Buy now and get one for yourself and one for your loved one, so you can share and synchronize your photo album together</h5>
-                <div className="grid grid-cols-3 gap-12 mb-5">
+                <div className="grid grid-cols-3 gap-3 mb-5 mx-auto">
                     {
                         producuts.map(
                             (item, index)=><Product 
